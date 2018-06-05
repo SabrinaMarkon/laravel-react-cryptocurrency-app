261ddcdd1563d87c295b97f019413e702dbe0400
Sabrina Markon <phpsitescripts@outlook.com>
2018-06-05 02:18:48 -0600

    Create Root component for SPA

--- resources/assets/src/app.js
+++ resources/assets/src/app.js
@@ -4,13 +4,28 @@
  * includes React and other helpers. It's a great starting point while
  * building robust, powerful web applications using React + Laravel.
  */
-require('./app.scss');
-require('./bootstrap');
+import './app.scss';
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import Root from './components/Root';
 
 /**
  * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+// require('./components/Example');
 
-require('./components/Example');
+/*
+We’re using BrowserRouter instead of HashRouter because we’re rendering app view against every possible route and we’re sure about it. We use HashRouter in situations where we have limited access to the server or serving our app statically via index.html  file.
+*/
+if($('#root').length) {
+    ReactDOM.render(
+        <BrowserRouter>
+            <Root/>
+        </BrowserRouter>
+        document.getElementById('root');
+    );
+}
---
+++ resources/assets/src/components/Root.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Route} from 'react-router';
+import CurrencySidebar from './CurrencySidebar';
+import Currency from './Currency';
+import './Currencies.scss';
+
+class Root extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currencies: []
+        }
+        /* using coinmarketcap API here to fetch cryptocurrencies data. */
+        this.api_url = 'https://api.coinmarketcap.com/v1/ticker/?limit=20';
+    }
+
+    /*
+    In componentDidMount()  lifecycle method, I’m fetching top 20 cryptocurrencies from API and updating Root component’s currencies state.  CurrencySidebar  component takes currencies prop and renders them.
+    */
+    componentDidMount() {
+        $.ajax({
+            type: 'GET',
+            url: this.api_url,
+            dataType: 'json',
+            crossDomain: true,
+            success: function(response) {
+                this.setStat({
+                    currencies: response
+                })
+            }.bind(this)
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Route path="/" render={(props) => (
+                    <CurrencySlider
+                        currencies={this.state.currencies}
+                        {...props}
+                        />
+                )} />
+                <Route exact path="/currency/:id" render={(props) => (
+                    <Currency {...props} />
+                )} />
+            </div>
+        );
+    }
+}
+
+export default Root;
